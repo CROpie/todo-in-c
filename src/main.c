f1c9b4f70b5b8a5c1b84f881939d07a9e7e30059
CROpie <96667010+CROpie@users.noreply.github.com>
2025-04-02 22:32:43 +1100

    Step 1: manual todo creation and print

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_MESSAGE_LENGTH 129 
+
+typedef struct {
+  int id;
+  char message[MAX_MESSAGE_LENGTH];
+} Todo;
 
 int main() {
-  printf("Yo, Peanut!");
+  Todo* todo = (Todo*) malloc(sizeof(Todo));
+  todo->id = 1;
+  strcpy(todo->message, "Yo, Peanut!");
+  printf("todo message: %s\n", todo->message);
+  return 1;
 }

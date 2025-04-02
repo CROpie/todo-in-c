8d8bb8df4433ec5917dd21110ac22ed268c3e6a1
CROpie <96667010+CROpie@users.noreply.github.com>
2025-04-02 23:01:34 +1100

    Step 2: print todoList

--- src/main.c
+++ src/main.c
@@ -3,16 +3,39 @@
 #include <string.h>
 
 #define MAX_MESSAGE_LENGTH 129 
+#define MAX_TODOS 10
 
 typedef struct {
   int id;
   char message[MAX_MESSAGE_LENGTH];
 } Todo;
 
+typedef struct {
+  int numTodos;
+  Todo* todos[MAX_TODOS];
+} TodoList;
+
+void printTodo(Todo* todo) {
+    printf("todo message %d: %s\n", todo->id, todo->message);
+}
+
+void printAllTodos(TodoList* todoList) {
+  for (int i = 1; i < todoList->numTodos + 1; i++) {
+    printTodo(todoList->todos[i]);
+  }
+}
+
 int main() {
-  Todo* todo = (Todo*) malloc(sizeof(Todo));
-  todo->id = 1;
-  strcpy(todo->message, "Yo, Peanut!");
-  printf("todo message: %s\n", todo->message);
+  TodoList* todoList = (TodoList*) malloc(sizeof(TodoList));
+  todoList->numTodos = 0;
+  int count = 6;
+  for (int i = 1; i < count; i++) {   
+    Todo* todo = (Todo*) malloc(sizeof(Todo));
+    todo->id = i;
+    strcpy(todo->message, "Yo, Peanut!");
+    todoList->todos[i] = todo;
+    todoList->numTodos++;
+  }
+  printAllTodos(todoList);
   return 1;
 }
